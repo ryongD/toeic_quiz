b8c642eefccfaf2ecf9edd2528047bf16ea304ba
ryongD <noviceDevelop@gmail.com>
2025-06-19 23:13:38 +0900

    picture size modified

--- src/pages/ToeicDay2Quiz.tsx
+++ src/pages/ToeicDay2Quiz.tsx
@@ -117,7 +117,11 @@ function ToeicQuiz() {
   return (
     <div className="max-w-3xl mx-auto p-6 space-y-12">
       <div className="text-center mb-6">
-        <img src="/daryong.png" alt="한다룡이 영어 정복하러 간다" className="mx-auto w-20 h-auto" />
+        <img
+           src="/public/daryong.png"
+           alt="귀여운 이미지"
+           className="block mx-auto w-20 h-auto"
+        />
       </div>
       <h1 className="text-3xl font-bold text-center mb-6">📘 TOEIC Day 2 퀴즈</h1>
 
